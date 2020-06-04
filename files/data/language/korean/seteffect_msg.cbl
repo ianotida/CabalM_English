fa56ba2b46ec8291c655681dca8a4a97a1796168
Ian <38063617+ianotida@users.noreply.github.com>
2020-06-05 00:21:02 +0300

    enc files update

---
+++ files/data/language/korean/seteffect_msg.cbl
@@ -0,0 +1,54 @@
+<seteffect_message>
+	<version	index="1"	/>
+		<msg	id="set_effect0001"	cont="수련용"	/>
+		<msg	id="set_effect0002"	cont=" "	/>
+		<msg	id="set_effect0003"	cont="강화"	/>
+		<msg	id="set_effect0004"	cont="실크"	/>
+		<msg	id="set_effect0005"	cont="아이언"	/>
+		<msg	id="set_effect0006"	cont="섀도우스틸"	/>
+		<msg	id="set_effect0007"	cont="아라미드"	/>
+		<msg	id="set_effect0008"	cont="블루스틴"	/>
+		<msg	id="set_effect0009"	cont="티타니움"	/>
+		<msg	id="set_effect0010"	cont="섀도우 티타니움"	/>
+		<msg	id="set_effect0011"	cont="오스미움"	/>
+		<msg	id="set_effect0012"	cont="미스틱"	/>
+		<msg	id="set_effect0013"	cont="샤인가드"	/>
+		<msg	id="set_effect0014"	cont="테라그레이스"	/>
+		<msg	id="set_effect0015"	cont="시그메탈"	/>
+		<msg	id="set_effect0016"	cont="미스릴"	/>
+		<msg	id="set_effect0017"	cont="드라이 프라메"	/>
+		<msg	id="set_effect0018"	cont="질서의 드라이 프라메 에펄릿"	/>
+		<msg	id="set_effect0019"	cont="전사의 드라이 프라메 에펄릿"	/>
+		<msg	id="set_effect0020"	cont="현자의 드라이 프라메 에펄릿"	/>
+		<msg	id="set_effect0021"	cont="장인의 블루스틴"	/>
+		<msg	id="set_effect0022"	cont="장인의 티타니움"	/>
+		<msg	id="set_effect0023"	cont="장인의 섀도우 티타니움"	/>
+		<msg	id="set_effect0024"	cont="장인의 오스미움"	/>
+		<msg	id="set_effect0025"	cont="장인의 미스틱"	/>
+		<msg	id="set_effect0026"	cont="장인의 샤인가드"	/>
+		<msg	id="set_effect0027"	cont="장인의 테라그레이스"	/>
+		<msg	id="set_effect0028"	cont="장인의 시그메탈"	/>
+		<msg	id="set_effect0029"	cont="장인의 미스릴"	/>
+		<msg	id="set_effect0030"	cont="명인의 블루스틴"	/>
+		<msg	id="set_effect0031"	cont="명인의 티타니움"	/>
+		<msg	id="set_effect0032"	cont="명인의 섀도우 티타니움"	/>
+		<msg	id="set_effect0033"	cont="명인의 오스미움"	/>
+		<msg	id="set_effect0034"	cont="명인의 미스틱"	/>
+		<msg	id="set_effect0035"	cont="명인의 샤인가드"	/>
+		<msg	id="set_effect0036"	cont="명인의 테라그레이스"	/>
+		<msg	id="set_effect0037"	cont="명인의 시그메탈"	/>
+		<msg	id="set_effect0038"	cont="명인의 미스릴"	/>
+		<msg	id="set_effect0039"	cont="아크리디움"	/>
+		<msg	id="set_effect0040"	cont="장인의 아크리디움"	/>
+		<msg	id="set_effect0041"	cont="명인의 아크리디움"	/>
+		<msg	id="set_effect0042"	cont="카오스 아머"	/>
+		<msg	id="set_effect0043"	cont="카오스 배틀"	/>
+		<msg	id="set_effect0044"	cont="카오스 마샬"	/>
+		<msg	id="set_effect0045"	cont="팔라디움"	/>
+		<msg	id="set_effect0046"	cont="장인의 팔라디움"	/>
+		<msg	id="set_effect0047"	cont="명인의 팔라디움"	/>
+		<msg	id="set_effect0048"	cont="수호의 드라이 프라메 에펄릿"	/>
+		<msg	id="set_effect0049"	cont="데모나이트"	/>
+		<msg	id="set_effect0050"	cont="모험가"	/>
+		<msg	id="set_effect0051"	cont="라이트"	/>
+</seteffect_message>
